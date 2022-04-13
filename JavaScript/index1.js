c4cbc289328180a612c1a632ea04b0baad981b7e
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-14 00:09:21 +0530

    diff. ways to render selector

--- JavaScript/index1.js
+++ JavaScript/index1.js
@@ -1,1 +1,5 @@
-ReactDOM.render(<h1></h1>)
+// ReactDOM.render(<p>Hi, i am learning React!</p>, document.querySelector("#root"));
+ReactDOM.render(<h1>Welcome to React...!</h1>, document.getElementById('root'));
+/*****we can write getElementById instead of querySelector*/
+
+// ReactDOM.render(<ul><li>1st item</li><li>1st item</li><li>1st item</li></ul>, document.getElementById('root'));
