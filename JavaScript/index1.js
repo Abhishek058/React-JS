770c373249db9aee90f265d6d64383f38db5221a
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-14 23:10:47 +0530

    updated

--- JavaScript/index1.js
+++ JavaScript/index1.js
@@ -2,8 +2,10 @@
 // ReactDOM.render(<h1>Welcome to React...!</h1>, document.getElementById('root'));
 /*****we can write getElementById instead of querySelector*/
 
+/******creating nested tags */
 // ReactDOM.render(<ul><li>1st item</li><li>1st item</li><li>1st item</li></ul>, document.getElementById('root'));
 
+/*******creating 2 elements in 1 div */
 // function Navbar() {
 //   return (<h1>new file added</h1>);
 // }
@@ -12,9 +14,10 @@
 //   return (<h1>I am learning React!</h1>);
 // }
 
-// ReactDOM.render(<div><Navbar /><MainContent /></div>, document.getElementById('root'));
+ReactDOM.render(<div><Navbar /><MainContent /></div>, document.getElementById('root'));
 
-const h1 = document.createElement("h1");
-h1.textContent = "This is created using JS";
-h1.className = "header";
-document.getElementById("root").append(h1);
+/******Create tag using javascript */
+// const h1 = document.createElement("h1");
+// h1.textContent = "This is created using JS";
+// h1.className = "header";
+// document.getElementById("root").append(h1);
