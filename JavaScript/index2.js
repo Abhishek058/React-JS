d5a2d58ddf3a6605f19beeb5774b872f9228b26a
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-14 23:34:12 +0530

    navbar created

--- JavaScript/index2.js
+++ JavaScript/index2.js
@@ -1,1 +1,26 @@
-ReactDOM.render(<h1>This is JSX</h1>, document.getElementById("root"));
+// const divroot = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is pharagraph</p>
+//     </div>
+// )
+
+// ReactDOM.render(
+//     divroot,
+//     document.getElementById("root")
+// );
+
+const navb = (
+    <nav>
+        <h1>Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navb, document.getElementById("navbar")
+);
