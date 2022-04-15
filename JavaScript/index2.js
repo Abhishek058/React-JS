49dc15ad35d2e22d47d02566b7e52d5a043fd549
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-15 07:59:45 +0530

    updated

--- JavaScript/index2.js
+++ JavaScript/index2.js
@@ -1,14 +1,14 @@
-// import React from "react"
-
 const navb = (
-  <nav>
-    <h1>HalfCoder</h1>
-    <ul>
-      <li>Menu</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-);
-
-ReactDOM.render(navb, document.getElementById("navbar"));
+    <nav>
+        <h1>HalfCoder</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+  
+  ReactDOM.render(
+    navb, document.getElementById("navbar")
+  );
