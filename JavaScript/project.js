fc809bcd283c36fbc05e1e692ea3ab1323c080b1
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-19 07:37:38 +0530

    project completed

--- JavaScript/project.js
+++ JavaScript/project.js
@@ -1,21 +1,36 @@
 function Navbar() {
   return (
-    <nav className="nav">
-      {/* <img src="https://lh3.googleusercontent.com/ivXWlwY0b-og_bhjPLFh76N9w34s4iYQZMv4RQpDuLPWtMHaBxqoEYyVKaCFvXVPuZCtzu1X3HSKbi39rZ-5BjDXAw2hmgEpW5JOZkMgGzl-PsXByMq7LFziXkKbJyL1My4nsv-zyOz-q34O7nKYxRhY_bAmqXUxJkcYe5KoRkh5yFh-ylcJ5echfazTMkNJgGLbifDKKtvH0YwK1amVCLkxnh4zG_R6303kmjjYT8aR3pMdrrMJHB62ANZJlEfInh2ODM4EP5Rq4dQJqW5uGoK03pzF11o50bvjmjcg5Db9Fopv3RvYmvrCzx853BStE0zg1b4q_fx-R7vYC2mq8liV0zGo9KG3Di19P-luuEzdJl1S_ucYPzr5lqmQ9_vFrZunEcWtSQLjzKrtIEzvXNnQvy4gNI4jVAFmJnPfFDnDQYPsTwCppyzrpvNUXufKYmO57obiO2xn751O5h0rShhbI5I2owXkURQQCA9yyr0pWkwWEapx_aoEFB1G1jp1Qb4yxPb1rpqUb_oPEuZH2rkYTD_cUiltJWP-6e7uHzELMtGJYHXHG6wuL5naMBfkOQVDwtvedcGmWXp9AqfWooz-VRyUsKmwvUHXKQhjbbvQnhN8enOwc4n6sX8mYVNp3X8Sq--zPE-uO_JmafqknBBlnvW3-T-FWYYrorNqa8B1JRL9Hi1-ngWHtPx92Cocyfv7jbT6LaGWuK8Za-zy0olQvNwX4aYVBeOt83n3ph3cGfINaxiwxEwc-1pg=w681-h578-no?authuser=0"></img> */}
-      <h1>navbar component</h1>
+    <nav>
+      <img
+        src="https://lh3.googleusercontent.com/UKX7QFsF7f5UaWus2pRUfTb-lXso2ad7vrPCk8a6rJOKw7pjhw_4JBvYsCmwkmTLnA2MMxdUIaTbBmeTzBw28e9DsGmlra7f-xFTvMIGriz3NzWfl4twkBPr8WCdwRr18GmFhw18ksViM9LoT6eGyLPwpZdelGbp_qBsbhtV3ciBAnQRtF3nhhhaJTWaqlhBEI1md45sNoAiH-LIYiCL11Wg0klTduIFzrkop4-UmIKhv8Om9tWbOdQanEfhpCTdP2_7IP6bu_TxF6zLfAgjMGOc06qWD1A2oMBVFBAlz9IlV1Xueh7Qh3PrT1EwTFKUlb22_vzugGsFONz0lsTRBw_0iUDe5bc1mj6vM0u4-9qpYAjHg4g1frKwvbj3-EeRueDRVZn3zj9GLcZFqpQL-D7rXGFCtnbm58E7AEXuJ_vW8jIyUlot6bS2_FoCm_lKnMT1dHJQ9yWSrKCFFOeUhbwsvL5LHH7WvoSo02ZTPb7_3sK7FR24bT2R_claMSLrQ-iec7wJFa7L08NLp79KQWFarL9thhSZZOlohOQ_WxtCm8roW0tdB9ZcSqHBhSMD98mBjYUTl7i_8iJUrBdVUAbUiniaswJootez4n9r2EHTZzWD_TJ7If9kiKi75GbyTfaBV2oBCfE9U79GR1uwbWikebu4ZQRks446Gjv8dktekg_1f-TdTb3KD8ZZA9UCU7dT_Msb2OW_aYWf_uevUM7AuIczN1hhsBC9lNh8clFbCVpD02KPVx7WXOVo=w543-h460-no?authuser=0"
+        width="50px"
+      ></img>
+      <h2>ReactFacts</h2>
+      <h3>React Course-Project 1</h3>
     </nav>
   );
 }
 
 function Main() {
-  return <h1>Main component</h1>;
+  return (
+    <div class="main">
+      <h1>Fun facts about react</h1>
+      <ul>
+        <li>React is not a framework</li>
+        <li>React has a strong community</li>
+        <li>Virtual DOM is faster than real DOM</li>
+        <li>React development and SEO go together</li>
+        <li>Keep your components small</li>
+        <li>Supports Type Systems</li>
+      </ul>
+    </div>
+  );
 }
 
 function App() {
   return (
-    <div className="">
+    <div>
       <Navbar />
-
       <Main />
     </div>
   );
