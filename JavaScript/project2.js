4fc9946a50a224c929ee3fb1d454943eefa2a49e
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-19 23:29:53 +0530

    navbar created

--- JavaScript/project2.js
+++ JavaScript/project2.js
@@ -1,6 +1,17 @@
 function Navbar() {
-    return (
-      <h1>App Component</h1>
-    );
-  }
-  
+  return (
+    <nav>
+      <img src="https://lh3.googleusercontent.com/RMz5PMi5IrrB-kSb_2dIHV-3xT4xipaZ-9duApCp6e3KsUg9xTv42tLc6PE3O1VkXhYwzOXKdobSDugSW0l2PozJzAOLraNVRwlfenKC66M6vCQV8U4LU6T_MwZU2aFKiOsl64wTpfLOJ_pJ1C999yv41aI5EMbL_KUR77joko77j54FI9fHBJq70kcCgjsO6nwljg7Q98__Fn_LK-eUwbG6mvc9wXLM2RE1zqSbQKa5pGBW4NMpC60gcngwkXCyBG0_NRy5Rrgy7FZRsakqQHt5yXNK9Bs8bF_NMdQzrgzyMzQQiX2aaJzNNehgxeRUvNqssYW2DkvutXLvOCr36zG04S_8rNV5KgT-FXN5h6o6HZrQFGIkLpOt3SLFmTHd3qJLNwagBI2gtwKFWOcQgTlABwKHTZ3WuE4IDF8yNeIUX_SmOWu5OxzLvGh7eMn02kJGxjYvZgxGDHW6nIQu232h5X_7x39ec0SV8IQVGviagLQY66xCSYZ_JP5zQ3nVCNqrJIrMwu_9rBCPBLVv0K4kqLFSiPuDVMyvembwW3ihcogTLIUuD8FaLRS53VSvQGgkC9-PJyo-6GieqFCVYvuaF9rZ4_-13Xg-YHg3d0wyRd0rjAEKoHY6Nsm7PnI1XuZsBoW9UJdjYRTZEEjJFV44FUvYMh6j_KeVxs5x2TK9_UQsflGaD9q1FtM63G_FISpJU-Bs7i4bQpAfavRKjjuYZLUqg_byrWDMpnBtI-0B4IUvofTTAFsGsO3y=w83-h26-no?authuser=0" />
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
