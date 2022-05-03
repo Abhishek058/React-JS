897d25af9db19cf922c755048b41a4539bd45f1d
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-03 09:36:41 +0530

    new commit

--- JavaScript/index4.js
+++ JavaScript/index4.js
@@ -1,32 +1,39 @@
-function Header() {
-  return (
-    <div className="header">
-      <img className="header-img" src="https://lh3.googleusercontent.com/Af2TIsYu_FOC-1HV5PK1GSmg6I2hfPem5ES0Qodc-qZzXPjsgZTGwePGA11SlvAc5rX3G0iVv4uOxsuZuqDBljlJnFxhOO7j-gxBttu1BkJMaIGWjDLQ4FINwsUjIx7SH_pYbGnMtkkTTUhLtyvsbge-noZL9LNKGzseliMDy5xQxFK42gU2BvrSY0wSSczOWdv2GUCyWhy7sMUFNLRoazAmWV2VKT1uITT9XG19B0gfuFTMNxi-kOOT7TI-JbsPTnyDIn3_p2QUAlrtcqSTZ5hKnOV6SIhzBtNO29QBLXV1byec__2NkvaniYDI9mkI9AI-8OK2bzK2e1IVYA18MB7Ugt0ZXTUY44On8dTID0PvX-rfT3r409odY9dPiDAmE61wuK4vJInm84Lb1H9HQpprPhzHeKJYhDjpbbLrdl6pOJm9huZYZKi41G2V3OG-3G4r-SNkNyW71wfza-DhwSNfLj6FzzWN6F22nes8oNy9ocvJZ3_vQpo8oTs_Wxggv47qH8v1kBjBIRsvs3mOUX0N0lyeS3iwlnOH8oWjJoEO4dK33Pgbex7VaVO5hX9F_h9ESD0cc60XDWOewl1k-KXStJJeuvMZbwSBlMwnjg06Ys2cY0H1cc1TTXDGEFPmpE62PT0KothZi9c_ifa1J4vk3HSEGk70sBw2Vo8i5daPO2BfD8uv46QBGL2J_2r9sAtRLcqbsFZxNDgvB1qIOAMKuiFic-Vfr4XJY8BNRTEbeWGIJfApFpY9LeuJ0A=w126-h105-no?authuser=0" />
-      <h1 className="header-h1">Meme Generetor</h1>
-      <h3 className="header-h3">Make your own meme and share on social media</h3>
-    </div>
-  );
-}
-
-function Meme(){
-  return (
+import React from "react";
+import memeget from "./memeget";
+export default function meme(){
+  function Header() {
+    return (
+      <div className="header">
+        <img className="header-img" src="https://lh3.googleusercontent.com/Af2TIsYu_FOC-1HV5PK1GSmg6I2hfPem5ES0Qodc-qZzXPjsgZTGwePGA11SlvAc5rX3G0iVv4uOxsuZuqDBljlJnFxhOO7j-gxBttu1BkJMaIGWjDLQ4FINwsUjIx7SH_pYbGnMtkkTTUhLtyvsbge-noZL9LNKGzseliMDy5xQxFK42gU2BvrSY0wSSczOWdv2GUCyWhy7sMUFNLRoazAmWV2VKT1uITT9XG19B0gfuFTMNxi-kOOT7TI-JbsPTnyDIn3_p2QUAlrtcqSTZ5hKnOV6SIhzBtNO29QBLXV1byec__2NkvaniYDI9mkI9AI-8OK2bzK2e1IVYA18MB7Ugt0ZXTUY44On8dTID0PvX-rfT3r409odY9dPiDAmE61wuK4vJInm84Lb1H9HQpprPhzHeKJYhDjpbbLrdl6pOJm9huZYZKi41G2V3OG-3G4r-SNkNyW71wfza-DhwSNfLj6FzzWN6F22nes8oNy9ocvJZ3_vQpo8oTs_Wxggv47qH8v1kBjBIRsvs3mOUX0N0lyeS3iwlnOH8oWjJoEO4dK33Pgbex7VaVO5hX9F_h9ESD0cc60XDWOewl1k-KXStJJeuvMZbwSBlMwnjg06Ys2cY0H1cc1TTXDGEFPmpE62PT0KothZi9c_ifa1J4vk3HSEGk70sBw2Vo8i5daPO2BfD8uv46QBGL2J_2r9sAtRLcqbsFZxNDgvB1qIOAMKuiFic-Vfr4XJY8BNRTEbeWGIJfApFpY9LeuJ0A=w126-h105-no?authuser=0" />
+        <h1 className="header-h1">Meme Generetor</h1>
+        <h3 className="header-h3">Make your own meme and share on social media</h3>
+      </div>
+    );
+  }
+  
+  function getMemeImg(){
+    // const 
+  }
+  
+  function Meme(){
+    return (
+      <div className="form-div">
+          <input className="form-text" type="text" placeholder="Type Upper Text" />
+          <input className="form-text" type="text" placeholder="Type Lower Text" />
+          <button className="form-btn" onClick={getMemeImg}>Generate new Meme 🖼️</button>
+      </div>
+    )
+  }
+  
+  function App() {
+    return(
     <div>
-      <form className="form">
-        <input className="form-text2" type="text" placeholder="Type Upper Text" />
-        <input className="form-text1" type="text" placeholder="Type Lower Text" />
-        <button className="form-btn">Generate Meme 🖼️</button>
-      </form>
+      <Header />
+      <Meme />
     </div>
-  )
+    )
+  }
+  
+  ReactDOM.render(<App />, document.getElementById("root"));
 }
 
-function App() {
-  return(
-  <div>
-    <Header />
-    <Meme />
-  </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
